68b86e107ee053020ae6670e9aa1e8a24b5c03bb
oceanbao <oceanbao@outlook.com>
2023-08-23 19:26:29 +0800

    fix: CICD v2

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -43,7 +43,10 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 	w.Header().Set("Content-Type", "application/json")
 	// NOTE: the order of these are vital: Header() before WriteHeader() before Write()
 	w.WriteHeader(status)
-	w.Write(js)
+	_, err = w.Write(js)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
